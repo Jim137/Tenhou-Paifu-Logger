7aa2e2fd183dfb23a2377e0bdff8cb1aedb74318
Jim137 <jiangjim@gapp.nthu.edu.tw>
2023-02-02 01:40:09 +0800

    encoding fixed in local.py

--- src/local.py
+++ src/local.py
@@ -5,7 +5,7 @@ from .localizations.local_str import local_str
 def localized_str(lang: str):
     localized = local_str()
     try:
-        with open(f'./src/localizations/{lang}.json') as f:
+        with open(f'./src/localizations/{lang}.json', encoding='utf-8') as f:
             data = json.load(f)
             localized.load_data(data)
     except FileNotFoundError:
