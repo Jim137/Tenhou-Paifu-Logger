46538f8a9201b076f738c4fcedbd8ae1fca44808
Jim137 <jiangjim@gapp.nthu.edu.tw>
2023-02-02 15:58:04 +0800

    localized file update

--- src/local.py
+++ src/local.py
@@ -1,15 +1,30 @@
 import json
-from .localizations.local_str import local_str
 
+class local_str():
+    def __init__(self):
+        pass
+
+    def load_data(self, data):
+        for key in data:
+            self.__setattr__(key, data[key])
+
+    def __setattr__(self, name, val):
+        super().__setattr__(name, val)
+
+    def __getattr__(self, name):
+        import json
+        with open('./localizations/en.json') as f:
+            data = json.load(f)
+        return data[name]
 
 def localized_str(lang: str):
     localized = local_str()
     try:
-        with open(f'./src/localizations/{lang}.json', encoding='utf-8') as f:
+        with open(f'./localizations/{lang}.json', encoding='utf-8') as f:
             data = json.load(f)
             localized.load_data(data)
     except FileNotFoundError:
-        with open('./src/localizations/en.json') as f:
+        with open('./localizations/en.json') as f:
             data = json.load(f)
             localized.load_data(data)
     return localized
