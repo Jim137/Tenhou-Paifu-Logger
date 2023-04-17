92fd35c2487a48ff162bce404d9ac34539e902a2
Jim137 <jiangjim@gapp.nthu.edu.tw>
2023-04-18 02:45:07 +0800

    skip duplicated match

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,4 @@ from .get_place import get_place
 from .local import localized_str
 from .log_into_xlsx import log_into_xlsx
 from .log_into_html import log_into_html
+from .url_log import url_log, check_duplicate
