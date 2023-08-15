68ed2967bebe62ea22f5cbb28494d4cf665b2bc2
Jim137 <jiangjim@gapp.nthu.edu.tw>
2023-08-15 21:45:21 +0800

    log2mjai import hint

--- paifulogger/src/get_paifu.py
+++ paifulogger/src/get_paifu.py
@@ -1,8 +1,6 @@
 import xml.etree.ElementTree as ET
 import os
 
-from .mjlog2mjai.parse import load_mjlog, parse_mjlog_to_mjai
-
 from .url_request_handler import url_request_handler
 from .Paifu import Paifu
 from .local import local_str
@@ -18,10 +16,19 @@ def get_paifu(url: str, local_str: local_str, mjai: bool = False):
     url = url.split('=')[1]+'='+url.split('=')[2]
     with open(f'./{local_str.paifu}/{paifu.go_str}/'+url+'.xml', 'w') as t:
         t.write(response)
+    
+    # mjai format output
     if mjai:
         if paifu.player_num == 3:
-            print('3人麻雀は未対応です')
+            print(local_str.sanma_mjai_error)
+            return paifu
+        
+        try:
+            from .mjlog2mjai.parse import load_mjlog, parse_mjlog_to_mjai
+        except ImportError:
+            print(local_str.log2mjai_import_error)
             return paifu
         with open (f'./{local_str.paifu}/{paifu.go_str}/'+url+'.mjson', 'w', encoding='UTF-8') as f:
             f.write(parse_mjlog_to_mjai(load_mjlog(f'./{local_str.paifu}/{paifu.go_str}/'+url+'.xml')))
+
     return paifu
