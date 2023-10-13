2e5c431711408ce5608de368950afca4107585de
Jim137 <jim@mail.jim137.eu.org>
2023-10-14 04:08:58 +0800

    csv write row

--- paifulogger/src/log_into_csv.py
+++ paifulogger/src/log_into_csv.py
@@ -1,9 +1,11 @@
+import csv
 from datetime import datetime
 import re
 
 from .get_place import get_place
 from .Paifu import Paifu
 from .i18n import local_str
+
 
 def log_into_csv(paifu: Paifu, local_str: local_str, output: str):
     try:
@@ -12,5 +14,27 @@ def log_into_csv(paifu: Paifu, local_str: local_str, output: str):
         else:
             paifu_str = local_str.yonma + local_str.paifu
         path = f"{output}/{local_str.paifu}/{paifu_str}.csv"
+        csvfile = open(path, "a+")
+        writer = csv.writer(csvfile)
     except FileNotFoundError:
-        pass
+        csvfile = open(path, "w")
+        writer = csv.writer(csvfile)
+        writer.writerow(
+            [
+                local_str.date,
+                local_str.plc,
+                local_str.paifu,
+                local_str.remark,
+                local_str.preR,
+            ]
+        )
+    writer.writerow(
+        [
+            datetime.strptime(re.findall(r"\d{10}", paifu.url)[0], "%Y%m%d%H"),
+            get_place(paifu, paifu.ban),
+            paifu.url,
+            "",
+            float(paifu.r[paifu.ban]),
+        ]
+    )
+    return None
