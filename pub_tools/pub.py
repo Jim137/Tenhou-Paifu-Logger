5948a8b2845c0252d0f8a9aa083f2f29f8182ac9
Jim137 <jim@mail.jim137.eu.org>
2024-04-12 19:56:20 +0800

    Add GitHub Actions workflow for  publishing

---
+++ pub_tools/pub.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+from paifulogger import __version__
+
+
+def build_pyproject():
+    if "dev" in __version__:
+        print("Dev version, not building.")
+        return None
+
+    with open("requirements.txt", "r") as f:
+        requirements = [f"{line.strip()}" for line in f.readlines()]
+
+    pyproject = f"""[build-system]
+requires      = ["setuptools>=61.0.0", "wheel"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "PaifuLogger"
+version = "{__version__}"
+description = "Logging tenhou paifu into excel or html file with some key information."
+readme = "README.md"
+authors = [{{ name = "Jim137", email = "jim@mail.jim137.eu.org" }}]
+license = {{ file = "LICENSE" }}
+classifiers = [
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+]
+keywords = ["mahjong", "review", "logger", "paifu", "tenhou", "haifu", "paipu", "riichi-mahjong"]
+dependencies = {requirements}
+requires-python = ">=3.10"
+
+[project.urls]
+Homepage = "https://github.com/Jim137/Tenhou-Paifu-Logger"
+
+[project.scripts]
+log = "paifulogger.__main__:main"
+plog = "paifulogger.__main__:main"
+pdl = "paifulogger.paifu_dl:paifu_dl"
+
+[tool.setuptools.packages.find]
+exclude = [
+    "Paifu",
+    "牌譜",
+]
+
+[tool.setuptools.package-data]
+"paifulogger.localizations" = [
+    "*.json",
+]""".replace(
+        "'", '"'
+    )
+
+    with open("pyproject.toml", "w") as f:
+        f.write(pyproject)
+
+
+if __name__ == "__main__":
+    print(__version__)
+    build_pyproject()
