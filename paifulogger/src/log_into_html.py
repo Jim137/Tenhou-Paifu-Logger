8863c3e3142a0c966d3fcbc4f9a777010490d44e
Jim137 <jim@mail.jim137.eu.org>
2024-04-12 18:43:58 +0800

    reformat `log_into_html.py`

--- paifulogger/src/log_into_html.py
+++ paifulogger/src/log_into_html.py
@@ -8,22 +8,22 @@ from .i18n import LocalStr
 from .Paifu import Paifu
 
 
-class html():
+class html:
     def __init__(self, html_str: str, paifu_str, local_lang: LocalStr):
-        if html_str.split('</tbody>')[0] == '':
-            self.logged = ''
+        if html_str.split("</tbody>")[0] == "":
+            self.logged = ""
             self.create_html(paifu_str, local_lang)
         else:
-            self.logged = html_str.split('</tbody>')[0]
-        self.new_log = ''
-        self.end_table = ''
-        if html_str.split('</body>') [0] == '':
-            self.replay = ''
-        elif len(html_str.split('</body>')) < 2:
-            self.replay = ''
+            self.logged = html_str.split("</tbody>")[0]
+        self.new_log = ""
+        self.end_table = ""
+        if html_str.split("</body>")[0] == "":
+            self.replay = ""
+        elif len(html_str.split("</body>")) < 2:
+            self.replay = ""
         else:
-            self.replay = html_str.split('</body>')[1]
-        self.end = '</body></html>'
+            self.replay = html_str.split("</body>")[1]
+        self.end = "</body></html>"
         pass
 
     def __repr__(self) -> str:
@@ -65,9 +65,8 @@ class html():
         """
 
     def log_into_table(self, paifu: Paifu):
-        time_str = datetime.strptime(re.findall(
-            r'\d{10}', paifu.url)[0], "%Y%m%d%H")
-        self.new_log += f'''
+        time_str = datetime.strptime(re.findall(r"\d{10}", paifu.url)[0], "%Y%m%d%H")
+        self.new_log += f"""
                     <tr>
                         <td>{time_str}</td>
                         <td>{paifu.get_place(paifu.ban)}</td>
@@ -75,11 +74,12 @@ class html():
                         <td><textarea id="persisted-text"></textarea></td>
                         <td>{float(paifu.r[paifu.ban])}</td>
                     </tr>
-        '''
+        """
 
     def average_plc(self, local_lang: LocalStr):
         html_p = (
-            self.logged + self.new_log 
+            self.logged
+            + self.new_log
             + """
                     </tbody>
                 </table>
@@ -92,13 +92,14 @@ class html():
         avg_plc = df[f"{local_lang.plc}"].mean()
         return avg_plc
 
-
     def end_of_table(self, local_lang: LocalStr):
-        self.end_table += f"""
+        self.end_table += (
+            f"""
                 </tbody>  
             </table>
             <p>{local_lang.avg_plc} = {self.average_plc(local_lang)}</p>
-            """+"""
+            """
+            + """
             <script>
                 if (window.localStorage) {
                     var p = document.querySelector('#persisted-text');
@@ -112,6 +113,7 @@ class html():
             </script>
         </body>
         """
+        )
 
 
 def log_into_html(paifu: Paifu, local_lang: LocalStr, output: str):
@@ -125,11 +127,15 @@ def log_into_html(paifu: Paifu, local_lang: LocalStr, output: str):
             html_str = f.read()
         html_c = html(html_str, paifu_str, local_lang)
     except FileNotFoundError:
-        html_c = html('', paifu_str, local_lang)
+        html_c = html("", paifu_str, local_lang)
     html_c.log_into_table(paifu)
     html_c.end_of_table(local_lang)
-    with open(path, 'w', encoding='utf-8') as f:
+    with open(path, "w", encoding="utf-8") as f:
         f.write(repr(html_c))
-    print('html: '+ local_lang.hint_record1 +
-          re.findall(r'\d{10}gm-\w{4}-\w{4}-\w{8}&tw=\d', paifu.url)[0] + local_lang.hint_record2)
+    print(
+        "html: "
+        + local_lang.hint_record1
+        + re.findall(r"\d{10}gm-\w{4}-\w{4}-\w{8}&tw=\d", paifu.url)[0]
+        + local_lang.hint_record2
+    )
     return None
